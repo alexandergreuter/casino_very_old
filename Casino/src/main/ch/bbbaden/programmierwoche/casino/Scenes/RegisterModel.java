56eef2fc9c50d8716d2dfb4aa9b54414906867ce
XelaaleX1234 <32978721+Xelaalex1234@users.noreply.github.com>
2019-04-08 09:36:27 +0200

    removed old files

--- Casino/src/main/ch/bbbaden/programmierwoche/casino/Scenes/RegisterModel.java
+++
@@ -1,9 +0,0 @@
-package main.ch.bbbaden.programmierwoche.casino.Scenes;
-
-import main.ch.bbbaden.programmierwoche.casino.Model;
-
-class RegisterModel extends Model {
-    RegisterModel() {
-        super("/fxml/register.fxml", "register", true);
-    }
-}
