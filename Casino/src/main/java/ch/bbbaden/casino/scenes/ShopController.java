7fc7dee4b36cbdf4f8992a27a40c33d6b92906a2
XelaaleX1234 <32978721+Xelaalex1234@users.noreply.github.com>
2019-04-09 16:54:15 +0200

    Changed about nothing

---
+++ Casino/src/main/java/ch/bbbaden/casino/scenes/ShopController.java
@@ -0,0 +1,11 @@
+package ch.bbbaden.casino.scenes;
+
+import ch.bbbaden.casino.Controller;
+import ch.bbbaden.casino.Model;
+
+public class ShopController implements Controller {
+
+    public void update(Model model) {
+        
+    }
+}
