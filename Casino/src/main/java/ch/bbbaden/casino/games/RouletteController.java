1946f2f7a1f6f9eb8fa2392b763302876066ef38
LijanMay <49271226+LijanMay@users.noreply.github.com>
2019-04-11 16:55:58 +0200

    Buttons programmiert

--- Casino/src/main/java/ch/bbbaden/casino/games/RouletteController.java
+++ Casino/src/main/java/ch/bbbaden/casino/games/RouletteController.java
@@ -7,7 +7,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -15,6 +18,9 @@ import java.util.ResourceBundle;
 
 public class RouletteController implements Controller, Initializable {
 
+    public Button besbutt;
+    public Button abbbutt;
+    public AnchorPane anchorPane;
     private RouletteModel rouletteModel;
     private ArrayList<Button> buttons = new ArrayList<Button>();
     @FXML
@@ -22,28 +28,49 @@ public class RouletteController implements Controller, Initializable {
 
     public void update(Model model) {
         rouletteModel = (RouletteModel) model;
-        int rows = gridPane.getRowConstraints().size();
-        int columns = gridPane.getColumnConstraints().size();
-
-        for (int row = 0; row < rows; row++) {
-            for (int column = 0; column < columns; column++) {
-                Button button = new Button("Text");
-                button.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        handleButtonAction((Button) e.getSource());
-                    }
-                });
-                button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                gridPane.add(button, column, row);
-                System.out.println(row);
-            }
-        }
     }
 
-    private void handleButtonAction(Button button) {
+    private void handleButtonAction(Button button, ActionEvent actionEvent) {
+        System.out.println(button);
+        ImageView iv = new ImageView("/images/jeton-red.png");
+        System.out.println(GridPane.getColumnIndex(button));
+        System.out.println(GridPane.getRowIndex(button));
+        gridPane.add(iv, GridPane.getColumnIndex(button), GridPane.getRowIndex(button));
+        //anchorPane.getChildren().add(iv);
+        //iv.setX(button.getLayoutX() + button.getParent().getLayoutX());
+        //iv.setY(button.getLayoutY() + button.getParent().getLayoutY());
+        iv.setScaleX(0.1);
+        iv.setScaleY(0.1);
     }
 
     public void initialize(URL location, ResourceBundle resources) {
 
+        int rows = gridPane.getRowConstraints().size();
+        int columns = gridPane.getColumnConstraints().size();
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                Button button = new Button("Text");
+                gridPane.add(button, column, row);
+                button.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        handleButtonAction((Button) e.getSource(), e);
+                    }
+                });
+                button.setOpacity(0);
+                button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            }
+        }
+    }
+
+    public void abbmethod(ActionEvent actionEvent) {
+        Stage stage = (Stage) abbbutt.getScene().getWindow();
+        stage.close();
+    }
+
+    public void jet50method(ActionEvent actionEvent) {
+
+    }
+
+    public void jet25method(ActionEvent actionEvent) {
     }
 }
