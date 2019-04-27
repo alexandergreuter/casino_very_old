679a09e929844a8da3c97a7b5a7e2c59356038e8
XelaaleX1234 <32978721+Xelaalex1234@users.noreply.github.com>
2019-04-27 13:51:09 +0200

    Merge branch 'master' of C:\Users\greut\OneDrive - Alte Kantonsschule Aarau\Documents\Development\Java\BBB-Game\Casino with conflicts.

--- Casino/src/main/java/ch/bbbaden/casino/MainApp.java
+++ Casino/src/main/java/ch/bbbaden/casino/MainApp.java
@@ -1,18 +1,22 @@
 package ch.bbbaden.casino;
 
+import ch.bbbaden.casino.games.PennyPusherModel;
 import ch.bbbaden.casino.scenes.StartModel;
 import javafx.application.Application;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class MainApp extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        new ModelManager(stage).setModel(new StartModel());
-    }
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws SQLException {
+        NormalUser normalUser = new NormalUser();
+        normalUser.login("OTTO", "OTTO");
+        new ModelManager(stage).setModel(new PennyPusherModel(normalUser));
     }
 }
