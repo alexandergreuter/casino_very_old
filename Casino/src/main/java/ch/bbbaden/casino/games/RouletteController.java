b8ae8c5d5134eda0b6f5f9ca38067ebecf9f2c45
LijanMay <49271226+LijanMay@users.noreply.github.com>
2019-04-29 16:58:44 +0200

    Jetons werden ausgewählt

--- Casino/src/main/java/ch/bbbaden/casino/games/RouletteController.java
+++ Casino/src/main/java/ch/bbbaden/casino/games/RouletteController.java
@@ -17,10 +17,17 @@ import java.util.ArrayList;
 
 public class RouletteController implements Controller {
 
+    public ImageView jeton100;
+    SelectedJeton selectedJeton;
+
     public Button besbutt;
     public Button abbbutt;
     public AnchorPane anchorPane;
     public ImageView jeton5;
+    public ImageView jeton25;
+    public ImageView jeton50;
+    public ImageView jeton1001;
+    public ImageView jeton10;
     private RouletteModel rouletteModel;
     private ArrayList<Button> buttons = new ArrayList<Button>();
     @FXML
@@ -73,7 +80,45 @@ public class RouletteController implements Controller {
         }
     }
 
-    public void jeton5click(MouseEvent mouseEvent) {
-        jeton5.setImage(new Image("/images/Roulette_Bilder/Jetons5-Ausgewählt.png"));
+
+
+    public void handleJeton5(MouseEvent mouseEvent) {
+        jeton5.setImage(new Image("/images/Roulette_Bilder/Jetons-5-Ausgewählt.png"));
+        jeton10.setImage(new Image("/images/Roulette_Bilder/jetons-10.png"));
+        jeton25.setImage(new Image("/images/Roulette_Bilder/jetons-25.png"));
+        jeton50.setImage(new Image("/images/Roulette_Bilder/jetons-50.png"));
+        jeton100.setImage(new Image("/images/Roulette_Bilder/jetons-100.png"));
+    }
+
+    public void handleJeton10(MouseEvent mouseEvent) {
+        jeton10.setImage(new Image("/images/Roulette_Bilder/Jetons-10-Ausgewählt.png"));
+        jeton5.setImage(new Image("/images/Roulette_Bilder/Jetons-5.png"));
+        jeton25.setImage(new Image("/images/Roulette_Bilder/jetons-25.png"));
+        jeton50.setImage(new Image("/images/Roulette_Bilder/jetons-50.png"));
+        jeton100.setImage(new Image("/images/Roulette_Bilder/jetons-100.png"));
+    }
+
+    public void handleJeton25(MouseEvent mouseEvent) {
+        jeton25.setImage(new Image("/images/Roulette_Bilder/Jetons-25-Ausgewählt.png"));
+        jeton5.setImage(new Image("/images/Roulette_Bilder/Jetons-5.png"));
+        jeton10.setImage(new Image("/images/Roulette_Bilder/jetons-10.png"));
+        jeton50.setImage(new Image("/images/Roulette_Bilder/jetons-50.png"));
+        jeton100.setImage(new Image("/images/Roulette_Bilder/jetons-100.png"));
+    }
+
+    public void handleJeton50(MouseEvent mouseEvent) {
+        jeton50.setImage(new Image("/images/Roulette_Bilder/Jetons-50-Ausgewählt.png"));
+        jeton5.setImage(new Image("/images/Roulette_Bilder/Jetons-5.png"));
+        jeton10.setImage(new Image("/images/Roulette_Bilder/jetons-10.png"));
+        jeton25.setImage(new Image("/images/Roulette_Bilder/jetons-25.png"));
+        jeton100.setImage(new Image("/images/Roulette_Bilder/jetons-100.png"));
+    }
+
+    public void handleJeton100(MouseEvent mouseEvent) {
+        jeton100.setImage(new Image("/images/Roulette_Bilder/Jetons-100-Ausgewählt.png"));
+        jeton5.setImage(new Image("/images/Roulette_Bilder/Jetons-5.png"));
+        jeton10.setImage(new Image("/images/Roulette_Bilder/jetons-10.png"));
+        jeton25.setImage(new Image("/images/Roulette_Bilder/jetons-25.png"));
+        jeton50.setImage(new Image("/images/Roulette_Bilder/jetons-50.png"));
     }
 }
